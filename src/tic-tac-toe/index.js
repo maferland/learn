95873b5fbc27d3318d032ad858e064143bc90e3e
Marc-Antoine Ferland <maferland@cortexstudio.com>
2018-04-30 20:48:05 -0400

    Add error message for overwrite

--- src/tic-tac-toe/index.js
+++ src/tic-tac-toe/index.js
@@ -30,28 +30,60 @@ class Game {
     console.log();
   }
 
+  isOver() {
+    const rows = Array.from({ length: size }, (v, i) => this.getRow(i));
+    const cols = Array.from({ length: size }, (v, i) => this.getCol(i));
+    const diagonals = [];
+
+    const lines = rows.concat(cols).concat(diagonals);
+
+    let over = false;
+    for (line in lines) {
+      if (new Set(line).length > 1 && line[0] != tokens.empty) {
+        over = true;
+        break;
+      }
+    }
+
+    if(over) {
+      console.log('Somebody has won!');
+    }
+    else if(this.turn === 9) {
+      console.log(`It's a tie!`)
+    }
+
+    return over;
+  }
+
   getRow(row) {
     const firstIndex = row * size;
     return this.grid.slice(firstIndex, firstIndex + size);
   }
 
+  getCol(col) {
+    return Array.from({ length: size },
+      (_, i) =>
+        this.grid(this.getIndex(col, i))
+    );
+  }
+
   play(x, y, player) {
     const index = this.getIndex(x, y);
-    if(this.grid[index] != tokens.empty) {
+    if (this.grid[index] != tokens.empty) {
       console.log('This cell is already taken. Play again!');
       return;
     }
     const token = player == 0 ? tokens.first : tokens.second;
     this.grid[index] = token;
     ++turn;
+
+    if(this.isOver()) {
+      console.log('Game over');
+    }
   }
 
   getIndex(x, y) {
     return x + y * size;
-  }
-
-  isOver() {
-    
   }
 }
 
@@ -64,3 +96,6 @@ game.printGrid();
 game.play(0, 0, 1);
 game.play(0, 1, 1);
 game.printGrid();
+
+game.play(1, 0, 0);
+game.play(2, 0, 0);
