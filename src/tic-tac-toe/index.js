f2562082225d4cf21339d9c0ebbcb429b8d9ce0e
Marc-Antoine Ferland <maferland@cortexstudio.com>
2018-04-30 20:25:38 -0400

    Default grid printing

--- src/tic-tac-toe/index.js
+++ src/tic-tac-toe/index.js
@@ -8,7 +8,32 @@ const tokens = {
 // Tic Tac Toe game
 class Game {
   constructor() {
-    cellCount = Math.pow(size, 2);
-    grid = Array.from(tokens.empty.repeat(this.cellCount));
+    this.cellCount = Math.pow(size, 2);
+    this.grid = Array.from(tokens.empty.repeat(this.cellCount));
+  }
+
+  printGrid() {
+    for (let i = 0; i < size; i++) {
+      this.printRow(i);
+    }
+  }
+
+  printRow(rowNum) {
+    const row = this.getRow(rowNum);
+    for (let i = size - 1; i >= 1; --i) {
+      row.splice(i, 0, '|');
+    }
+
+    row.forEach((token) => process.stdout.write(token));
+    console.log();
+  }
+
+  getRow(row) {
+    const firstIndex = row * size;
+    return this.grid.slice(firstIndex, firstIndex + size);
   }
 }
+
+
+const game = new Game();
+game.printGrid();
