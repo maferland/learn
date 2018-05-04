5e6a9e811133a7c488fda03ca1109c69169d9e46
Marc-Antoine Ferland <maferland@cortexstudio.com>
2018-05-03 22:51:42 -0400

    Add twelve days problem

--- src/diamond/diamond.js
+++ src/diamond/diamond.js
@@ -1,59 +1,49 @@
-class Diamond {
+module.exports = class Diamond {
   constructor() {
     this.letters = Array.from('ABCDEFGHIJKLMNOPQRSTUVXYZ');
   }
 
   makeDiamond(letter) {
     const index = this.letters.indexOf(letter);
-
-    if (index === 0) {
-      return 'A\n';
-    }
-
+    const symmetricalLevel = index - 1;
     const rows = [];
-    for (let i = 0; i <= index; i++) {
-      rows.push(this.makeRow(i, index));
+    for (let i = 0; i < index; i++) {
+      rows.push(makeRow(i));
     }
 
-    for (let i = index - 1; i >= 0; --i) {
+    for(let i = index - 2; i <= 0; i--) {
       rows.push(rows[i]);
     }
 
     let output = '';
 
     rows.forEach((row) => {
-      output = output.concat(row.concat('\n'));
+      output.concat(row.concat('\n'));
     });
 
-    return output;
+    return outp
   }
 
   makeRow(current, max) {
+    if (max === 1) {
+      return 'A';
+    }
+
     const arround = ' '.repeat(max - current);
 
-    if (current === 0) {
+    if (current === 1) {
       return `${arround}A${arround}`;
     }
 
     const letter = this.letters[current];
-    const width = 2 * max + 1;
+    const width = 2 * max - 1;
 
     if (current === max) {
-      const middle = ' '.repeat(width - 2);
+      const middle = ' '.repeat(width -2);
       return `${letter}${middle}${letter}`;
     }
 
-    const middle = ' '.repeat(width - 2 - (arround.length * 2));
+    const middle = ' '.repeat(width - 2 - (arround * 2));
     return `${arround}${letter}${middle}${letter}${arround}`
   }
 }
-
-module.exports = Diamond;
-
-const diamond = new Diamond();
-
-console.log(diamond.makeDiamond('A'));
-console.log(diamond.makeDiamond('B'));
-console.log(diamond.makeDiamond('C'));
-console.log(diamond.makeDiamond('D'));
-console.log(diamond.makeDiamond('E'));
